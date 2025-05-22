76d6099026312c6f923ca751d422c95d1f0376c4
EzzedeenKhaled <ezzedeenkhaled7@gmail.com>
2025-05-22 20:49:03 +0300

    Deploy

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import postRoutes from './routes/postRoutes.js'
 import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
-
+const PORT = process.env.PORT || 8080;
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
     try {
         connectDB(process.env.MONGO_URL)
-        app.listen(8080, () => {
+        app.listen(PORT, () => {
             console.log(`Server started on port http://localhost:8080`)
         })
     } catch (error) {
